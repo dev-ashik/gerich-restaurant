cdb4ece78352e7499e12412e6d3809a9416f0e6f
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2023-01-22 14:20:36 +0800

    last update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components';
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar/>
+      <Header/>
+      <AboutUs/>
+      <SpecialMenu/>
+      <Chef/>
+      <Intro/>
+      <Laurels/>
+      <Gallery/>
+      <FindUs/>
+      <Footer/>
     </div>
   );
 }
 
+
+// Time: 34:49
 export default App;
---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '1rem' }}>
+    SubHeading
+  </div>
+);
+
+export default SubHeading;
